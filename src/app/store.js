4e3d3c0d61571b28fad474deebe91af8353cdbd6
Ulrick Preben Hansen <ulrick.preben.hansen@live.dk>
2023-09-28 17:08:09 +0200

    api folder and file added

---
+++ src/app/store.js
@@ -0,0 +1,8 @@
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+//Import the components reducers here
+
+export default configureStore({
+    reducer: {
+        //some reducers fx topics: topicsReducer,
+    },
+});
