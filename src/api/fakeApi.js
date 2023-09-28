b9eafd6b93d5dbb4e6d3974426dd3d632785f03c
Ulrick Preben Hansen <ulrick.preben.hansen@live.dk>
2023-09-28 17:35:07 +0200

    fakeApi added

---
+++ src/api/fakeApi.js
@@ -0,0 +1,13 @@
+const fakeData = {
+    data: {
+        children: [ 
+            {
+                data: {
+                    subreddit: "UlricksReddit",
+                    title: "Ooh he is making some fake data for reddit project!!!",
+                    
+                }
+            }
+        ]
+    }
+}
