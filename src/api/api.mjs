f447d8f2581f8b4f5c449a01ba20e513d6c80b47
Ulrick Preben Hansen <ulrick.preben.hansen@live.dk>
2023-10-18 18:48:49 +0200

    fething Popular reddit as inistial when app starts

---
+++ src/api/api.mjs
@@ -0,0 +1,41 @@
+//should be used for real API fetch
+// læs de sidste 2 svar for de seneste 2 spørgsmål her -> https://chat.openai.com/c/96668b13-7f69-40c1-a35d-af3fa86af37f
+
+export const searchReddit = (query) => {
+    return fetch(`https://www.reddit.com/search.json?q=${query}`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      });
+  };
+
+  /*searchReddit('cake%20recipes')
+  .then(result => {
+    console.log(result.data.children[0]);
+  })
+  .catch(error => {
+    console.error(error);
+  });*/
+
+export const initialReddit = () => {
+    return fetch(`https://www.reddit.com/r/popular.json`)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then((data) => {
+            return data.data.children;
+        });
+};
+
+initialReddit()
+.then(result => {
+    console.log(result[0])
+})
+.catch(error => {
+    console.error(error);
+});
