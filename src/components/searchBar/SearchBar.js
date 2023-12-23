b8ac6eb88e168f0069e69da09e025b128e6b403e
Ulrick Preben Hansen <ulrick.preben.hansen@live.dk>
2023-12-23 15:59:31 +0100

    SearchBar input type changed to search

--- src/components/searchBar/SearchBar.js
+++ src/components/searchBar/SearchBar.js
@@ -37,7 +37,7 @@ const SearchBar = () => {
         <div className="searchInput">
             <input 
                 id="searchInput"
-                type="text"
+                type="search"
                 placeholder="Search..."
                 value={searchInput}
                 onChange={handleSearchInput}
