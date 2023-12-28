774507236d92ae951f5b50675766df5a9eccabf8
Ulrick Preben Hansen <ulrick.preben.hansen@live.dk>
2023-12-28 19:47:08 +0100

    const errorState = useSelector((state) => state.error) deleted from Errorstate.js

--- src/components/errorState/ErrorState.js
+++ src/components/errorState/ErrorState.js
@@ -1,10 +1,9 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { setErrorState } from "./errorStateSlice.js";
 
 const ReportErrorButton = () => {
   const dispatch = useDispatch();
-  const errorState = useSelector((state) => state.error)
 
   const handleErrorStateButton = () => {
     dispatch(setErrorState(true));
